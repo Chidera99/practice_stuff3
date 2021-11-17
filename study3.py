bfdaf578700fd3a24f079e771c904e4675d85efb
Chidera99 <cnc2688@g.rit.edu>
2021-11-17 03:44:48 -0500

    Second

--- study3.py
+++ study3.py
@@ -1,8 +1,14 @@
 def make_tuple(a, b ,c):
     return (a, b,c)
 
+
+def reverse_tuple(a_sequence):
+    a_sequence.sort(reverse=True)
+    return tuple(a_sequence)
+
 def main():
     print(make_tuple(1,2,3))
+    print(reverse_tuple([1,2,3,4]))
 
 if __name__=="__main__":
     main()

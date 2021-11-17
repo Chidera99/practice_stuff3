af91eee79855d08a6df90efde445fd8be1e4b501
Chidera99 <cnc2688@g.rit.edu>
2021-11-17 04:23:19 -0500

    fourth

--- study3.py
+++ study3.py
@@ -9,10 +9,21 @@ def reverse_tuple(a_sequence):
 def make_list(a,b,c):
     return [a,b,c]
 
+def nth_list(a_sequence,n):
+    a=[]
+    for i in range(0,len(a_sequence)-1):
+        if i%n==0:
+            a.append(a_sequence[i+1])
+    return a
+
+
 def main():
     print(make_tuple(1,2,3))
     print(reverse_tuple([1,2,3,4]))
     print(make_list("a","b","c"))
+    print(nth_list(range(1,11),2))
+    print(nth_list("Chidera is a girl",2))
+    print(nth_list((1,"chidera", True, 99),2))
 
 if __name__=="__main__":
     main()

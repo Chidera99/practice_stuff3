d2cecb22092a90eaffc0c47508aeb4d091b8243e
Chidera99 <cnc2688@g.rit.edu>
2021-11-17 04:27:33 -0500

    fifth

--- study3.py
+++ study3.py
@@ -16,6 +16,10 @@ def nth_list(a_sequence,n):
             a.append(a_sequence[i+1])
     return a
 
+def splice(a_list,b_list):
+    for i in b_list:
+        a_list.append(i)
+
 
 def main():
     print(make_tuple(1,2,3))
@@ -24,6 +28,11 @@ def main():
     print(nth_list(range(1,11),2))
     print(nth_list("Chidera is a girl",2))
     print(nth_list((1,"chidera", True, 99),2))
+    a_list=[1,2]
+    b_list=["a","b"]
+    print(a_list)
+    splice(a_list,b_list)
+    print(a_list)
 
 if __name__=="__main__":
     main()
